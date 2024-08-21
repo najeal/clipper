997689a0dac267ef13f9cd78349c9e1f2fe25e47
Nathan Haim <haim.nathan@icloud.com>
2024-08-21 21:51:38 +0200

    main: use cobra generation

--- cmd/clipper/main.go
+++ cmd/clipper/main.go
@@ -7,20 +7,14 @@ import (
 
 	"github.com/najeal/clipper/cmd/clipper/gen"
 	"github.com/najeal/clipper/icobra"
-	pkg "github.com/najeal/clipper/internal"
 	"github.com/urfave/cli/v2"
 )
-
-type cliGenerator interface {
-	Generate(specFile []byte, packageName string) ([]byte, error)
-}
 
 type Service struct{}
 
 func (*Service) Run(ctx *cli.Context) error {
 	const packageName = "cmd"
 	specPath := ctx.Args().Get(0)
-	cobraMode := ctx.Args().Get(1)
 
 	specFile, err := os.ReadFile(specPath)
 	if err != nil {
@@ -28,17 +22,15 @@ func (*Service) Run(ctx *cli.Context) error {
 		return gen.ExitReadManifestFailed
 	}
 
-	if cobraMode == "" {
-		var generator cliGenerator = pkg.NewCliGenerator()
-		codeFile, err := generator.Generate(specFile, packageName)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "failed to generate cli code file: %v\n", err)
-			return gen.ExitCliGenerationFailed
-		}
+	//	var generator cliGenerator = pkg.NewCliGenerator()
+	//	codeFile, err := generator.Generate(specFile, packageName)
+	//	if err != nil {
+	//		fmt.Fprintf(os.Stderr, "failed to generate cli code file: %v\n", err)
+	//		return gen.ExitCliGenerationFailed
+	//	}
 
-		fmt.Fprintln(os.Stdout, string(codeFile))
-		return nil
-	}
+	//	fmt.Fprintln(os.Stdout, string(codeFile))
+	//	return nil
 	cligen := icobra.NewCliGenerator()
 	_, err = cligen.Generate(specFile, packageName)
 	if err != nil {
